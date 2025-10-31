6370658c5e39ee26ae5157640ca140796290cd28
IkrashHaroon <ikrashharoon1236@gmail.com>
2025-10-31 23:17:47 +0500

    Chapter 01

---
+++ script.js
@@ -0,0 +1,16 @@
+// Question 1
+alert("Welcome to my website!");
+
+// Question 2
+alert("Error! Please enter a valid password.");
+
+// Question 3 (line break using \n)
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question 4 (two alerts in sequence)
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Question 5 (Run this manually in browser console)
+// Open browser → Right-click → Inspect → Console tab → Paste this line ↓
+// alert("Assalam u Alaikum,Jawan Pakistan");
